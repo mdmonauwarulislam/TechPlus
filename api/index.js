faf358acb80996df259d3f004f7dcb3a9553b488
Md Monauwarul Islam <121303308+mdmonauwarulislam@users.noreply.github.com>
2024-08-01 21:34:59 +0530

    create api routes

--- api/index.js
+++ api/index.js
@@ -1,5 +1,21 @@
 const express = require("express");
-const app = express()
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require('dotenv');
+const userRouter = require('./routes/user.router');
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGODB_URL)
+.then( () => {
+    console.log("MongoDb is connected successfully!!");
+})
+.catch((err) => {
+    console.log(err);
+});
+
+app.use('/api/user', userRouter);
+
 
 app.listen(3000, () => {
     console.log("Server is running at PORT 3000 !")
