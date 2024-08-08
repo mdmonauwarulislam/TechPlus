d65a89be02a7214c134b016198ed258b3f621591
Md Monauwarul Islam <121303308+mdmonauwarulislam@users.noreply.github.com>
2024-08-09 00:08:46 +0530

    Add  persist reducer

--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -1,32 +1,32 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialstate = {
-    currentUser : null,
-    error : null,
-    loading : false,
+    currentUser: null,
+    error: null,
+    loading: false,
 }
 
 const userSlice = createSlice({
-    name : 'user',
-    initialState : initialstate,
-    reducers : {
-        signinStart : (state) => {
+    name: 'user',
+    initialState: initialstate,
+    reducers: {
+        signinStart: (state) => {
             state.loading = true;
-            state.error  = null;
+            state.error = null;
         },
-        signinSuccess : (state, action) => {
+        signinSuccess: (state, action) => {
             state.loading = false;
             state.currentUser = action.payload;
             state.error = null;
         },
-        signinFailure : (state, action) => {
+        signinFailure: (state, action) => {
             state.loading = false;
             state.error = action.payload;
         },
-       
+
     }
 })
 
-export const { signinStart, signinSuccess, signinFailure} = userSlice.actions;
+export const { signinStart, signinSuccess, signinFailure } = userSlice.actions;
 
 export default userSlice.reducer;
